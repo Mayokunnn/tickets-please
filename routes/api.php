5290ea018ad7b8bd5fbf966c370090dd6f4d5298
Kelvin Mayokun Areola <kareola960@gmail.com>
2024-05-19 13:00:31 +0100

    Revoke authentication tokens

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 //https::localhost:3306/api
@@ -11,8 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::get('/tickets', function (){
-   return \App\Models\Ticket::all();
-});
 
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,4 +4,4 @@
 use App\Http\Controllers\Api\v1\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
